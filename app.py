7624a538294e718881147c8a48e3b5d8cb12e79f
Vasco Guedes <up202004395@g.uporto.pt>
2023-04-15 21:17:02 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from model import get_products_needed
+from model import get_products_needed, pred_best_vals
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,8 +13,10 @@ def predict():
     # # Call the Streamlit app's function with the input data
     output_data = get_products_needed([input_data['N'], input_data['P'], input_data['K']], input_data['culture'], input_data['area'])
 
+    best_vals = pred_best_vals(input_data['culture'])
+    
     # Return the output data as the response
-    return jsonify(output_data)
+    return jsonify({"data": output_data, "best": best_vals})
 
 if __name__ == '__main__':
     # Start the Flask app
