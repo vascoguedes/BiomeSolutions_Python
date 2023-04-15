1a516b90c710ae84d46762ff1d5f5d1dbd8cee77
Vasco Guedes <up202004395@g.uporto.pt>
2023-04-15 21:20:46 +0100

    upda

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def predict():
     best_vals = pred_best_vals(input_data['culture'])
     
     # Return the output data as the response
-    return jsonify([output_data, best_vals])
+    return jsonify(output_data)
 
 if __name__ == '__main__':
     # Start the Flask app
