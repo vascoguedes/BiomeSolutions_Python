a4c9654c28d2a4bde2c5ec039b7d1251df6c7674
Vasco Guedes <up202004395@g.uporto.pt>
2023-04-15 21:24:34 +0100

    upda

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def predict():
     best_vals = pred_best_vals(input_data['culture'])
     
     # Return the output data as the response
-    return best_vals
+    return output_data
 
 if __name__ == '__main__':
     # Start the Flask app
