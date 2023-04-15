2564ad3cbd3435be308becfed4c2ff5088197d1b
Vasco Guedes <up202004395@g.uporto.pt>
2023-04-15 15:54:17 +0100

    uncomment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-# from model import get_products_needed
+from model import get_products_needed
 
 app = Flask(__name__)
 
@@ -9,10 +9,10 @@ def predict():
     input_data = request.get_json()
 
     # # Call the Streamlit app's function with the input data
-    # output_data = get_products_needed([input_data['N'], input_data['P'], input_data['K']], input_data['culture'], input_data['area'])
+    output_data = get_products_needed([input_data['N'], input_data['P'], input_data['K']], input_data['culture'], input_data['area'])
 
     # Return the output data as the response
-    return jsonify("VASCO")
+    return jsonify(output_data)
 
 if __name__ == '__main__':
     # Start the Flask app
